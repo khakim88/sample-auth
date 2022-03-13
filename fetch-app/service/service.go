41b88790173a5d02a3a63f35bd3d94eeef5abd4e
BLUEBIRDGROUP\agus.khakim <agus.khakim@bluebirdgroup.com>
2022-03-13 18:54:18 +0700

    no message

--- fetch-app/service/service.go
+++ fetch-app/service/service.go
@@ -4,19 +4,13 @@ import (
 	"context"
 
 	"github.com/khakim88/sample-auth/fetch-app/model"
-	"github.com/khakim88/sample-auth/fetch-app/repository"
 )
-
-type resourceService struct {
-	repo repository.DBReaderWriter
-}
-
-func NewResourceService(repo repository.DBReaderWriter) resourceService {
-	return resourceService{
-		repo: repo,
-	}
-}
 
 type ResourceService interface {
 	GetResourceService(ctx context.Context) (resp *model.GetResourceResponse, err error)
+}
+
+func GetResourceService(ctx context.Context) (resp *model.GetResourceResponse, err error) {
+
+	return &model.GetResourceResponse{}, nil
 }
