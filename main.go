c3b86c529b07e93f66b21edc73b919d4dd773a98
BLUEBIRDGROUP\agus.khakim <agus.khakim@bluebirdgroup.com>
2022-03-13 17:14:41 +0700

    add auth

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+var mySigningKey = []byte("password123456")
+
+func auth(w http.ResponseWriter, r *http.Request) {
+	validToken, err := GenerateJWT()
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	fmt.Fprintf(w, validToken)
+}
+
+func handleRequest() {
+	http.HandleFunc("/", auth)
+
+	log.Fatal(http.ListenAndServe(":9000", nil))
+}
+
+func GenerateJWT() (string, error) {
+
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["authorized"] = true
+	claims["user"] = "khakim"
+	claims["exp"] = time.Now().Add(time.Minute * 30).Unix()
+
+	tokenString, err := token.SignedString(mySigningKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func main() {
+	fmt.Println("Simple auth")
+	handleRequest()
+
+}
